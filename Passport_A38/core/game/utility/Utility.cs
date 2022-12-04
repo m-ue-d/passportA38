7558d4259bb06c45fb42edffa5d0a331c4ab5195
Fabian Mild <fabian.mild@gmx.at>
2022-12-04 15:03:29 +0100

    Utility.cs changed

--- Passport_A38/core/game/utility/Utility.cs
+++ Passport_A38/core/game/utility/Utility.cs
@@ -13,16 +13,16 @@ public static class Utility
         var map = new Dictionary<int, string>
         {
             {1000,"M"},
-            {900, "CM"},
+            {900,"CM"},
             {500,"D"},
-            {400, "CD"},
+            {400,"CD"},
             {100,"C"},
             {90, "XC"},
             {50,"L"},
             {40, "XL"},
             {10,"X"},
             {9, "IX"},
-            {5,"I"},
+            {5,"V"},
             {4, "IV"},
             {1,"I"}
         };
